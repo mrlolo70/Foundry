ae2ea463b738672f88e111076e6a4c15ea21ea26
mateoc@github.uio.no <mateoc@ifi.uio.no>
2021-02-15 12:36:13 +0100

    fixed style for editor and added first css files

--- module/sheets/L5R4ItemSheet.js
+++ module/sheets/L5R4ItemSheet.js
@@ -1,4 +1,13 @@
 export default class L5R4ItemSheet extends ItemSheet {
+
+  static get defaultOptions() {
+    return mergeObject(super.defaultOptions, {
+      width: 530,
+      height: 340,
+      classes: ["l5r4", "sheet", "item"]
+    });
+  }
+
   get template() {
     return `systems/l5r4/templates/sheets/${this.item.data.type}-sheet.hbs`;
   }
